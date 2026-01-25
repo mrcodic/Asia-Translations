7704e30727b00ada55826d088ac0a871ce9b5c1a
Ali Emam <53233600+aliemam2264@users.noreply.github.com>
2026-01-25 21:11:43 +0200

    headings font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/includes/footer";
 import Navbar from "@/includes/navbar";
 import { scrollToSection } from "@/lib/scrollToSection";
 import Image from "next/image";
-import Link from "next/link";
+import heroImg from "../../public/hero-3.png";
 
 export default function Home() {
   return (
@@ -19,10 +19,10 @@ export default function Home() {
 
       {/* Home Section */}
       <section id="home" className="p-8 mx-auto">
-        <div className="flex flex-col-reverse md:flex-row justify-between gap-4">
+        <div className="flex flex-col-reverse lg:flex-row justify-between items-center gap-4">
           {/* Content Side */}
           <div className="flex flex-col items-start gap-8">
-            <h1 className="text-5xl text-[#1F3A4A] font-bold">Professional Asian Language Translation Services</h1>
+            <h1 className="text-5xl text-[#1F3A4A]">Professional Asian Language Translation Services</h1>
             <p className="text-[20px]">Reach your audience across Asia with Asia Translation and connect with them on a deeper level. Grow your global presence through translations that reflect not only the language, but the culture behind it.</p>
             <button className="bg-linear-to-l from-[#D4AF37] to-[#dac172] text-[#1F3A4A] hover:translate-y-1 font-semibold text-[20px] px-6 py-2 rounded-md transition cursor-pointer"
               onClick={() => scrollToSection("join-us")}>
@@ -31,9 +31,7 @@ export default function Home() {
           </div>
 
           {/* Hero Side */}
-          <div>
-            <Image src={"/hero.png"} alt="hero" width={1090} height={500} />
-          </div>
+            <Image src={heroImg} alt="hero" width={1200}/>
         </div>
         {/* Icons */}
         <div className="flex flex-wrap items-center gap-6">
@@ -46,7 +44,7 @@ export default function Home() {
 
       {/* About Us Section */}
       <AnimatedSection id="about-us" animation="left" className="flex flex-col gap-4 items-center p-8 py-20 mx-auto">
-        <h1 className="text-5xl text-[#1F3A4A] font-bold">About Asia Translations</h1>
+        <h1 className="text-5xl text-[#1F3A4A]">About Asia Translations</h1>
         <p className="text-[20px] text-center">Asia Translation is a certified language services provider with over a decade of experience helping businesses grow across global markets.
           With a trusted network of native-speaking linguists across Asia, we deliver accurate, culturally relevant translation and localization services.
           We help brands communicate clearly, authentically, and on time—every time.</p>
@@ -54,26 +52,26 @@ export default function Home() {
 
       {/* Services Section */}
       <AnimatedSection id="services" animation="right" className="flex flex-col gap-4 items-center p-8 py-20 mx-auto">
-        <h1 className="text-5xl text-[#1F3A4A] font-bold">Our Services</h1>
+        <h1 className="text-5xl text-[#1F3A4A]">Our Services</h1>
         <p className="text-[20px] text-center">Comprehensive translation solutions for all your business needs.</p>
         <CollapsibleServices />
       </AnimatedSection>
 
       {/* Industries Section */}
       <AnimatedSection id="industries" animation="zoom" className="flex flex-col gap-4 items-center p-8 py-20 mx-auto">
-        <h1 className="text-5xl text-[#1F3A4A] font-bold">Industries</h1>
+        <h1 className="text-5xl text-[#1F3A4A]">Industries</h1>
         <CollapsibleIndustries />
       </AnimatedSection>
 
       {/* Languages Section */}
       <AnimatedSection id="languages" animation="up" className="p-8 mx-auto">
-        <h1 className="text-5xl text-[#1F3A4A] font-bold text-center pb-4">Languages</h1>
+        <h1 className="text-5xl text-[#1F3A4A] text-center pb-4">Languages</h1>
         <Languages />
       </AnimatedSection>
 
       {/* Join Us Section */}
       <AnimatedSection id="join-us" animation="zoom" className="flex flex-col gap-4 items-center p-8 mx-auto">
-        <h1 className="text-5xl text-[#1F3A4A] font-bold text-center">Get a Free Quote for Your Global Needs</h1>
+        <h1 className="text-5xl text-[#1F3A4A] text-center">Get a Free Quote for Your Global Needs</h1>
         <QuoteForm />
       </AnimatedSection>
 
